c1fc4758907e13a2337e3836595a588e54110339
Marcus Downing <marcus@bang-on.net>
2015-05-26 12:47:34 +0100

    UNIX_TIMESTAMP() function

--- migrate/manual_fix.sql
+++ migrate/manual_fix.sql
@@ -4,3 +4,26 @@ update variable set value='s:6:"< Prev";' where name='colorbox_text_previous';
 
 --update variable set value='s:6:"Next »";' where name='colorbox_text_next';
 --update variable set value='s:6:"« Prev";' where name='colorbox_text_previous';
+
+
+-- UNIX_TIMESTAMP()
+exec('DROP FUNCTION UNIX_TIMESTAMP');
+
+exec('
+CREATE FUNCTION UNIX_TIMESTAMP (
+@ctimestamp datetime
+)
+RETURNS integer
+AS 
+BEGIN
+  /* Function body */
+  declare @return integer
+
+  SELECT @return = DATEDIFF(SECOND,{d ''1970-01-01''}, @ctimestamp)
+
+  return @return
+END
+');
+
+
+
